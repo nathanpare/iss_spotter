6b0bca96768e5256b484fb5632b548fd05e88ff6
Nathan Pare <nathanparebusiness@gmail.com>
2022-03-01 16:38:50 -0800

    iss spotter with callbacks

---
+++ index.js
@@ -0,0 +1,46 @@
+// index.js
+const { nextISSTimesForMyLocation } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('70.68.22.93', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
+// let coordz = { latitude: 49.222, longitude: -122.6181 };
+// fetchISSFlyOverTimes(coordz, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // print the data
+  printPassTimes(passTimes);
+});
